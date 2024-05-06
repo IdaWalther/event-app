945d0bd90b4954d95cb60962f901afb3756cd692
Ida Walther <idahwalther@icloud.com>
2024-05-06 09:10:57 +0200

    Fixed searchbar

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -4,10 +4,10 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Searchbar() {
     return (
-        <div className="searchbarContainer">
+        <section className="searchbarContainer">
              <FontAwesomeIcon icon={faMagnifyingGlass} className="magnifyingGlass" />
             <input className="searchBar" type="text" placeholder="" />
-        </div>
+        </section>
     )
 }
 

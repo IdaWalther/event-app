20012f37d2b48fd26a086fb5056721177ffa2a64
Ida Walther <idahwalther@icloud.com>
2024-05-12 13:54:48 +0200

    Removed console.log

--- src/components/eventbuyticket/Eventbuyticket.jsx
+++ src/components/eventbuyticket/Eventbuyticket.jsx
@@ -15,13 +15,7 @@ function Eventbuyticket({event}) {
         const currentTicketPrice = eventTickets[eventId]?.totalCost / currentTicketCount || 0;
         
         if (currentTicketCount > 0) {
-            console.log('Tickets added to order');
-            if (eventTickets[eventId]) {
-                console.log('Event already in cart');
-                addToCart(eventId, currentTicketCount, currentTicketPrice);
-            } else {
             addToCart(eventId, currentTicketCount, currentTicketPrice);
-            }
         }
     }
 
